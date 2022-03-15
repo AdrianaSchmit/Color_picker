fcae4a45dae5fb3827a42571e5b250557e27fd6d
AdrianaSchmit <schmit.adriana@gmail.com>
2022-03-15 09:44:21 +0000

    text contrast add to h2

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,8 +24,21 @@ function randomColors() {
     //Add the color to the bg
     div.style.backgroundColor = randomColor;
     hexText.innerText = randomColor;
+    
+        //making  contrast for hex text
+        checkTextContrast(randomColor, hexText);  
  
   });
   }
   
   randomColors();
+
+  function checkTextContrast(color, text) {
+    const luminance = chroma(color).luminance();
+    if (luminance > 0.5) {
+      text.style.color = "black";
+    } else {
+      text.style.color = "white";
+    }
+  }
+  
