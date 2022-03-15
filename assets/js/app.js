b2fa3a6cf591e86c5cc8181a967f754ce33cb6f5
AdrianaSchmit <schmit.adriana@gmail.com>
2022-03-15 11:18:10 +0000

    Added function to lock and unlock the color choosed

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,22 @@
 // variables
 
 var colorDivs = document.querySelectorAll(".color");
+var generateBtn = document.querySelector(".generate");
 var lockButton = document.querySelectorAll(".lock");
+let initialColors;
+
+
+// event listeners
+
+generateBtn.addEventListener("click", randomColors);
+
+
+lockButton.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    toggleLockButton(index);
+  });
+}); 
+
 
 //functions______________________________
   
@@ -12,16 +27,25 @@ function createcolor() {
     var hexColor = chroma.random();
     return hexColor;
 }
-/* var randomHex = createcolor();
-console.log(randomHex); */
+
+
 
 function randomColors() {
+  initialColors = [];
     colorDivs.forEach((div, index) => {
     // get the div children element
     var hexText = div.children[0];
     // declare random variable to get createcolor function
     var randomColor = createcolor();
     var icons = colorDivs [index].querySelectorAll(".lock");
+
+        //Add it to the array
+        if (div.classList.contains("locked")) {
+          initialColors.push(hexText.innerText);
+          return;
+        } else {
+          initialColors.push(chroma(randomColor).hex());
+        }
   
     //Add the color to the bg
     div.style.backgroundColor = randomColor;
@@ -29,19 +53,15 @@ function randomColors() {
     
         //making  contrast for hex text
         checkTextContrast(randomColor, hexText); 
+         //making  contrast for lock button
         for (icon of icons){
-          checkTextContrast(randomColor, icon); 
+        checkTextContrast(randomColor, icon); 
         }
       
   }); 
  
   }
 
-
-
-
-
-  
 
 
   function checkTextContrast(color, text) {
@@ -52,6 +72,15 @@ function randomColors() {
       text.style.color = "white";
     }
   }
+
+
+function toggleLockButton(index) {
+  const div = lockButton[index].parentElement.parentElement;
+  const icon = lockButton[index].firstChild;
+  icon.classList.toggle("fa-lock-open");
+  icon.classList.toggle("fa-lock");
+  div.classList.toggle("locked");
+}
 
 
 
