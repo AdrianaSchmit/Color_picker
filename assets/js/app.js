c32f93ac8dc2e9df856643451725a57d152482f9
AdrianaSchmit <schmit.adriana@gmail.com>
2022-03-15 09:43:26 +0000

    random colors added to background

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 
+// variables
 
+var colorDivs = document.querySelectorAll(".color");
 
 //functions______________________________
   
@@ -11,3 +13,19 @@ function createcolor() {
 }
 /* var randomHex = createcolor();
 console.log(randomHex); */
+
+function randomColors() {
+    colorDivs.forEach((div) => {
+    // get the div children element
+    var hexText = div.children[0];
+    // declare random variable to get createcolor function
+    var randomColor = createcolor();
+  
+    //Add the color to the bg
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+ 
+  });
+  }
+  
+  randomColors();
