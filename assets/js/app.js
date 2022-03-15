65a56e1c852a0a612a6056468941578c06ab1d03
AdrianaSchmit <schmit.adriana@gmail.com>
2022-03-15 09:40:56 +0000

    Added function to create color from chroma

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+
+
+
+//functions______________________________
+  
+
+
+function createcolor() {
+    var hexColor = chroma.random();
+    return hexColor;
+}
+/* var randomHex = createcolor();
+console.log(randomHex); */
